1d1d0614621659c4ba47e97e9ed1a44942813caa
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-30 10:56:15 +0000

    Navbar
    Included the different items for the Navbar component

--- scences/global/Navbar.jsx
+++ scences/global/Navbar.jsx
@@ -13,7 +13,31 @@ const Navbar = () => {
 
     return (
         <Box display="flex" alignItems="center" width="100%" height="60px" backgroundColor="rgba(255, 255, 255, 0.95)" color="black" position="fixed" top="0" left="0" zIndex="1">
-            
+            <Box width="80%" margin="auto" display="flex" justifyContent="space-between" alignItems="center">
+                <Box color={shades.secondary[500]} sx={{"&:hover": {cursor: "pointer"}}} onClick={() => navigate("/")}>
+                    ECOMMER
+                </Box>
+                <Box display="flex" justifyContent="space-between" columnGap="20px" zIndex="2">
+                    <IconButton sx={{ color: black }}>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton sx={{ color: black }}>
+                        <PersonOutline />
+                    </IconButton>
+                    <Badge badgeContent={cart.length} color="secondary" invisible={cart.length === 0} sx={{
+                        "& .MuiBadge-badge": {
+                            right: "5px", top: "5px", padding: "0 4px", height: "14px", minWidth: "13px"
+                        }
+                    }}>
+                        <IconButton onClick={() => dispatch(setIsCartOpen({}))} sx={{ color: black }}>
+                            <ShoppingBagOutlined />
+                        </IconButton>
+                    </Badge>
+                    <IconButton sx={{ color: black }}>
+                        <MenuOutlined />
+                    </IconButton>
+                </Box>
+            </Box>
         </Box>
     );
 };
