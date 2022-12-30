7c0ed9a9a1f247fc91cbb4969adead49d6b3f846
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-30 10:36:05 +0000

    Imports
    Including the necessary imports for the navigation bar

---
+++ scences/global/Navbar.jsx
@@ -0,0 +1,21 @@
+// Including the necessary imports
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton } from "@mui/material";
+import { PersonOutline, ShoppingBagOutlined, MenuOutlined, SearchOutlined } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme";
+import { setIsCartOpen } from "../../state";
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.cart);
+
+    return (
+        <Box display="flex" alignItems="center" width="100%" height="60px" backgroundColor="rgba(255, 255, 255, 0.95)" color="black" position="fixed" top="0" left="0" zIndex="1">
+            
+        </Box>
+    );
+};
+
+export default Navbar;
