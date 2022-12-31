6340e1675b755f0eb23d649eea3fc0a5b1689bb0
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-31 01:32:48 +0000

    CartMenu component
    Included the necessary imports for the CartMenu component

---
+++ scences/global/CartMenu.jsx
@@ -0,0 +1,10 @@
+// The necessary imports for the CartMenu
+import { Box, Button, IconButton, Typography } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-materila/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../../theme";
+import { decreaseCount, IncreaseCount, removeFromCart, setIsCartOpen } from "../../state";
+import { useNavigate } from "react-router-dom";
+
