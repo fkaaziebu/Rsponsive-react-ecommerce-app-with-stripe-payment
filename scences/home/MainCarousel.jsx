9612c831998ac9b442238908f08ef10563bc5348
fkaaziebu <frederickaziebu1998@gmail.com>
2023-01-02 18:00:59 +0000

    MainCarousel Component
    Done

--- scences/home/MainCarousel.jsx
+++ scences/home/MainCarousel.jsx
@@ -16,7 +16,35 @@ export const heroTextureImports = importAll(
 );
 
 const MainCarousel = () => {
-    
+    const isNonMobile = useMediaQuery("min-width:600px");
+
+    return (
+        <Carousel InfiniteLoop={true} showThumbs={false} showIndicators={false} showStatus={false}
+            renderArrowPrev={(onClickHandler, hasPrev, label) => (
+                <IconButton onClick={onClickHandler} sx={{ position: "absolute", top: "50%", left: "0", color: "white", padding: "5px", zIndex: "10" }}>
+                    <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+                </IconButton>
+            )}
+            renderArrowNext={(onClickHandler, hasNext, label) => (
+                <IconButton onClick={onClickHandler} sx={{ position: "absolute", top: "50%", right: "0", color: "white", padding: "5px", zIndex: "10" }}>
+                    <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+                </IconButton>
+            )}
+        >
+            {Object.values(heroTextureImports).map((texture, index) => (
+                <Box key={`carousel-image-${index}`}>
+                    <img src={texture} alt={`carousel-${index}`} style={{ width: "100%", height: "700px", objectFit: "cover", backgroundAttachment: "fixed" }} />
+                    <Box color="white" padding="20px" borderRadius="1px" textAlign="left" backgroundColor="rgb(0,0,0,0.4)" position="absolute" top="46%" left={isNonMobile ? "10%" : "0"} right={isNonMobile ? undefined : "240px"}>
+                        <Typography variant="h1">Summer Sale</Typography>
+                        <Typography fontWeight="bold" color={shades.secondary[300]} sx={{ textDecoration: "underline" }}>
+                            Discover More
+                        </Typography>
+                    </Box>
+                </Box>
+            ))
+            };
+        </Carousel>
+    );
 }
 
 export default MainCarousel;
