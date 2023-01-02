ebaf32aff6669a96a294beef43d80628e510cd4a
fkaaziebu <frederickaziebu1998@gmail.com>
2023-01-02 17:07:41 +0000

    Footer Component
    The basic footer component

--- scences/global/Footer.jsx
+++ scences/global/Footer.jsx
@@ -2,3 +2,8 @@ import { useTheme } from "@emotion/react";
 import { Box, Typography } from "@mui/material";
 import { shades } from "../../theme";
 
+const Footer = () => {
+
+}
+
+export default Footer;
