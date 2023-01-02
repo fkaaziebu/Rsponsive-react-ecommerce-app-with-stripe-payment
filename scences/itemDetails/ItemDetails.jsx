1857b62db3cd007bc6aa6951efd700f582cc7d90
fkaaziebu <frederickaziebu1998@gmail.com>
2023-01-02 20:49:03 +0000

    ItemDetails component
    Included the necessary imports and variables for the ItemDetails page

---
+++ scences/itemDetails/ItemDetails.jsx
@@ -0,0 +1,24 @@
+import { Box, Button, IconButton, Typography } from "@mui/material";
+import Tabs from "@mui/material/Tabs";
+import tab from "@mui/material/Tab";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Item from "../../components/Item";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../../theme";
+import { useDispatch } from "react-redux";
+
+const ItemDetails = () => {
+    const dispatch = useDispatch();
+    const { itemId } = useParams();
+    const [value, setValue] = useState("description");
+    const [count, setCount] = useState(1);
+    const [item, setItem] = useSate(null);
+    const [items, setItems] = useState([]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    }
+}
