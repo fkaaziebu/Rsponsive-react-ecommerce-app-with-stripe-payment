c40ab5f40a43c3ad23f8a0907ae013aecd43604c
fkaaziebu <frederickaziebu1998@gmail.com>
2023-01-03 02:12:39 +0000

    Home components
    Set up the home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scences/home/Home";
+// import ItemDetails from "./scences/itemDetails/ItemDetails";
+// import Checkout from "./scences/checkout/Checkout";
+// import Confirmation from "./scences/checkout/Confirmation";
+import Navbar from "./scences/global/Navbar";
 
+const ScrollToTop = () => {
+  const pathname = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+};
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="item/:itemId" element={<ItemDetails />} /> */}
+          {/* <Route path="checkout" element={<Checkout />} /> */}
+          {/* <Route path="checkout/success" element={<Confirmation />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
